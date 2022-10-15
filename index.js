c3da7550eecf905c558325b4ec7970b3361f5384
teterabob <gilbertsahumada@gmail.com>
2022-10-15 15:31:25 -0300

    dot env file updated

--- index.js
+++ index.js
@@ -2,14 +2,11 @@ const { Requester, Validator } = require("@chainlink/external-adapter");
 const { head } = require("request");
 require('dotenv').config()
 
-// Define custom error scenarios for the API.
-// Return true for the adapter to retry.
+
 const customError = (data) => {
   if (data.Response === "Error") return true;
   return false;
 };
-
-
 
 const createRequest = (input, callback) => {
   // The Validator helps you validate the Chainlink request data
@@ -24,20 +21,8 @@ const createRequest = (input, callback) => {
   const classFlight = input.data.classFlight;
   
 
-  //const endpoint = validator.validated.data.endpoint || "price";
-  //const url = `https://beta3.api.climatiq.io/travel/flights/BER&HAM&2&first`;
-  const url = "https://beta3.api.climatiq.io/travel/flights";
+  const url = process.env.API_ENDPOINT;
   
-
-  const params = {
-    legs: [{
-        from: from,
-        to: to,
-        passengers: parseInt(passengers),
-        classFlight: classFlight,
-      }]    
-  };
-
   const headers = {
     Authorization: `Bearer ${API_KEY}`
   }
@@ -56,21 +41,9 @@ const createRequest = (input, callback) => {
     }
   };
 
-  // The Requester allows API calls be retry in case of timeout
-  // or connection failure
+
   Requester.request(config, customError)
     .then((response) => {
-      // It's common practice to store the desired value at the top-level
-      // result key. This allows different adapters to be compatible with
-      // one another.
-      //response.data.result = Requester.validateResultNumber(response.data, [
-      //  tsyms,
-      //]);
-      /*
-      if(response.data.co2e){
-        response.data.co2e = parseInt(response.data.co2e)
-      } 
-      */     
       callback(response.status, Requester.success(jobRunID, response));
     })
     .catch((error) => {
@@ -78,24 +51,19 @@ const createRequest = (input, callback) => {
     });
 };
 
-// This is a wrapper to allow the function to work with
-// GCP Functions
 exports.gcpservice = (req, res) => {
   createRequest(req.body, (statusCode, data) => {
     res.status(statusCode).send(data);
   });
 };
 
-// This is a wrapper to allow the function to work with
-// AWS Lambda
+
 exports.handler = (event, context, callback) => {
   createRequest(event, (statusCode, data) => {
     callback(null, data);
   });
 };
 
-// This is a wrapper to allow the function to work with
-// newer AWS Lambda implementations
 exports.handlerv2 = (event, context, callback) => {
   createRequest(JSON.parse(event.body), (statusCode, data) => {
     callback(null, {
@@ -106,6 +74,5 @@ exports.handlerv2 = (event, context, callback) => {
   });
 };
 
-// This allows the function to be exported for testing
-// or for running in express
+
 module.exports.createRequest = createRequest;
